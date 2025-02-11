95bdd500bdd19ee16a9d0df8f297edb2fbd61712
Farhan Rob <rob.farhan@gmail.com>
2025-02-10 23:14:31 -0600

    Adding hanlice

---
+++ keyboards/hanlice/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x4E, 0xF3, 0xD5, 0x0F, 0x89, 0x54, 0xC3, 0x23}
+
+#define VIAL_UNLOCK_COMBO_ROWS {0, 2}
+#define VIAL_UNLOCK_COMBO_COLS {0, 14}
